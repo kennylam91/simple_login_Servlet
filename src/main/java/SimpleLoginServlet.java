54ba927d91f019f39e0e5d46ec94688a674ecb39
kennylam91 <phamvanlam1991@gmail.com>
2019-07-01 14:51:47 +0700

    move code to Get method + form method=get

--- src/main/java/SimpleLoginServlet.java
+++ src/main/java/SimpleLoginServlet.java
@@ -7,23 +7,23 @@ import java.io.PrintWriter;
 
 public class SimpleLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username= request.getParameter("username");
         String password= request.getParameter("password");
 
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<html>");
         if("admin".equals(username)&&
-        "admin".equals(password)){
+                "admin".equals(password)){
             printWriter.println("<h1>Welcome</h1>");
         }
         else{
             printWriter.println("Error!");
         }
         printWriter.println("</html>");
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
