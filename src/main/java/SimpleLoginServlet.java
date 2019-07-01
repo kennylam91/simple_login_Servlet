a570385d94520323505715766fb3932360853427
kennylam91 <phamvanlam1991@gmail.com>
2019-07-01 14:50:43 +0700

    login ok with Post method

--- src/main/java/SimpleLoginServlet.java
+++ src/main/java/SimpleLoginServlet.java
@@ -3,9 +3,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SimpleLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username= request.getParameter("username");
+        String password= request.getParameter("password");
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        if("admin".equals(username)&&
+        "admin".equals(password)){
+            printWriter.println("<h1>Welcome</h1>");
+        }
+        else{
+            printWriter.println("Error!");
+        }
+        printWriter.println("</html>");
 
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,20 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>$Title$</title>
-  </head>
-  <body>
-    <h1>Hello world</h1>
-  </body>
+</head>
+<body>
+<h1>Login:</h1>
+<form action="/login" method="post">
+    <div>
+        <label for="username">username</label>
+        <input type="text" id="username" name="username">
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input type="text" id="password" name="password">
+    </div>
+    <input type="submit" value="Login">
+</form>
+</body>
 </html>
